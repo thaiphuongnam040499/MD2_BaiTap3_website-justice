337bac4c1bbb280a1d5e69c9932159e554c9f885
thaiphuongnam040499 <thainamvinhuni@gmail.com>
2023-01-16 16:45:44 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header';
+import Carousel from './components/Carousel';
+import AboutStart from './components/AboutStart';
+import ServicesStart from './components/ServicesStart';
+import Appointmen from './components/Appointmen';
+import Features from './components/Features';
+import Action from './components/Action';
+import Team from './components/Team';
+import Testimonial from './components/Testimonial';
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+  {/* Header Start */}
+  <Header/>
+  {/* Header End */}
+  {/* Carousel Start */}
+    <Carousel/>
+  {/* Carousel End */}
+  {/* About Start */}
+  <AboutStart/>
+  {/* About End */}
+  {/* Services Start */}
+  <ServicesStart/>
+  {/* Services End */}
+  {/* Appointment Start */}
+  <Appointmen/>
+  {/* Appointment End */}
+  {/* Features Start */}
+  <Features/>
+  {/* Features End */}
+  {/* Action Start */}
+    <Action/>
+  {/* Action End */}
+  {/* Team Start */}
+  <Team/>
+  {/* Team End */}
+  {/* Testimonial Start */}
+  <Testimonial/>
+  {/* Testimonial End */}
+  {/* Footer Start */}
+  <Footer/>
+  {/* Footer End */}
+</>
     </div>
   );
 }
---
+++ src/components/AboutStart.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+export default class AboutStart extends Component {
+  render() {
+    return (
+        <div className="container-fluid py-5">
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-lg-5">
+              <img className="img-fluid rounded" src="img/about.jpg" alt="" />
+            </div>
+            <div className="col-lg-7 mt-4 mt-lg-0">
+              <h2
+                className="position-relative text-center bg-white text-primary rounded p-3 mt-4 mb-4 d-none d-lg-block"
+                style={{ width: 350, marginLeft: "-205px" }}
+              >
+                25 Years Experience
+              </h2>
+              <h6 className="text-uppercase">Learn About Us</h6>
+              <h1 className="mb-4">
+                We Provide Reliable And Effective Legal Services
+              </h1>
+              <p>
+                Invidunt lorem justo sanctus clita. Erat lorem labore ea, justo
+                dolor lorem ipsum ut sed eos, ipsum et dolor kasd sit ea justo. Erat
+                justo sed sed diam. Ea et erat ut sed diam sea ipsum est dolor
+              </p>
+              <a href="" className="btn btn-primary mt-2">
+                Learn More
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
